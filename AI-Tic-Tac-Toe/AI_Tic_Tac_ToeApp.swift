8558f3844ec214210dc9995b1283f731d563cdec
Jonathan T. Nielsen <44645695+SnaXD@users.noreply.github.com>
2022-03-15 20:03:30 +0100

    Initial Commit

---
+++ AI-Tic-Tac-Toe/AI_Tic_Tac_ToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AI_Tic_Tac_ToeApp.swift
+//  AI-Tic-Tac-Toe
+//
+//  Created by Jonathan T. Nielsen on 15/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct AI_Tic_Tac_ToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
